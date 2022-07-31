83a007dea1698314d32eb51d9262d77de90bef86
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 23:34:26 +0900

    Create 5-1_FavList.py

---
+++ 5-1_FavList.py
@@ -0,0 +1,8 @@
+FavSongs = ['song0','song1','song2','song3','song4']
+FavColors = ['red','orange','yellow','green','blue']
+FavFoods = ['food0','food1','food2','food3','food4']
+del FavSongs[3]
+FavColors.append('Black')
+print(FavSongs)
+print(FavColors)
+print(FavFoods[2:5])
