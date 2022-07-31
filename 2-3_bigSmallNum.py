ed90136609f41716da295dbcd922cad6f1a96e2f
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 23:20:32 +0900

    Create 2-3_bigSmallNum.py

---
+++ 2-3_bigSmallNum.py
@@ -0,0 +1,11 @@
+first = int(input('첫 번째 정수를 입력하시오: '))
+second = int(input('두 번째 정수를 입력하시오: '))
+if first > second:
+ print('큰 수: %d' %(first))
+ print('작은 수: %d' %(second))
+ 
+elif first < second:
+ print('큰 수: %d' %(second))
+ print('작은 수: %d' %(first))
+else:
+ print('두 수는 같습니다.'
