a05964cd72a4ff0839900e14ee346c8edf4df4b3
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 23:24:16 +0900

    Create 3-3_random.py

---
+++ 3-3_random.py
@@ -0,0 +1,6 @@
+import random
+first = random.randint(1,100)
+second = random.randint(1,100)
+print('발생된 첫 번째 난수: %d' %(first))
+print('발생된 두 번째 난수: %d' %(second))
+print('발생된 두 개의 난수 합: %d' %(first+second))
