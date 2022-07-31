8434029911d8b59c153db0ea83250d23368f7622
kingdayeon <105100662+kingdayeon@users.noreply.github.com>
2022-07-31 23:10:05 +0900

    Create 1-1_input.py

---
+++ 1-1_input.py
@@ -0,0 +1,6 @@
+name = input('이름이 무엇인가요? ')
+dept = input('전공 분야는 무엇인가요? ')
+exp = input('프로그램 언어를 배운 적이 있나요(예/아니오)? ')
+year = int(input('프로그램 언어를 배운 적이 있다면 몇 년 정도 인가요(0/1/2)? '))
+heard = input('프로그램 언어(C/C++, Java, Python) 중 들어본 것이 있나요? ')
+score = float(input('이번 학기 예상되는 학점의 평점은 어떻게 되나요? '))
